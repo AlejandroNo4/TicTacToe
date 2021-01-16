6661d02c52bea00ca45c96cb992568d44123eb06
Alejandro Contreras <kdta15@outlook.com>
2021-01-15 20:21:30 -0600

    Added more classes to the script file

--- bin/main.rb
+++ bin/main.rb
@@ -1,80 +1,39 @@
 #!/usr/bin/env ruby
 require_relative '../lib/core_game.rb'
-class Player
-  attr_accessor :name, :char
-  def initialize(char)
-    @char = char
-  end
-
-  def start
-    puts "Hello, what's your player name?"
-    @name = gets
-    puts "#{@name.chomp}, you will play as #{@char} \n\n"
-    puts
-  end
+def wrong_input
+  puts "I'm sorry, please choose a valid input:"
+  puts '- Must to be a number between 1 and 9'
+  puts '- Must to be a free space'
 end
 
-first = Player.new('X')
-first.start
-second = Player.new('O')
-second.start
-
-class GameDisplay
-  attr_accessor :position, :pos, :repeated
-  def initialize
-    @pos = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-    @position = 0
-    @choosed = []
-  end
-
-  def maps(current_ply)
-    @name = current_ply[0]
-    @character = current_ply[1]
-    puts "Now #{@name.chomp}, it's your turn, choose wisely \n\n"
-    puts " 1 | 2 | 3             #{@pos[0]} | #{@pos[1]} | #{@pos[2]} "
-    puts '---+---+---           ---+---+---'
-    puts " 4 | 5 | 6             #{@pos[3]} | #{@pos[4]} | #{@pos[5]} "
-    puts '---+---+---           ---+---+---'
-    puts " 7 | 8 | 9             #{@pos[6]} | #{@pos[7]} | #{@pos[8]} \n\n"
-    @is_valid = false
-    while @is_valid == false
-      @position = gets
-      if @position.to_i.zero? || @choosed.any? { |u| u == @position } || @position.to_i.between?(1, 9) == false
-        puts "Wait! Please enter a valid number\n"
-        redo
-      else
-        @choosed << @position
-        @pos[position.to_i - 1] = @character
-      end
-      return @is_valid = true
-    end
-  end
+def winner_msg(name)
+  puts "Congrats #{name.chomp} you have WON!!! :D"
 end
 
-class Start
-  attr_accessor :player_one, :player_two, :turn
-  def initialize(player_one, player_two)
-    @player_one = player_one
-    @player_two = player_two
-  end
-
-  def switch
-    between_this = [@player_one, @player_two].cycle
-    @round = GameDisplay.new
-    @check_if_win = CoreTest.new
-    i = 0
-    while i < 9
-      @turn = between_this.next
-      @round.maps(@turn)
-      @check_if_win.checker(@round.position, @turn[1])
-      return puts "#{@turn[0].chomp} WON!!! :D" if @check_if_win.test_res == true
-
-      i += 1
-      puts 'Tis is a tie, sorry' if i == 9
-    end
-  end
+def tie_message
+  puts "I'm sorry, this was a tie, good luck next time :)"
 end
 
-game = Start.new([first.name, first.char], [second.name, second.char])
+def map(in_turn, token, pos)
+  puts "Now #{in_turn.chomp}, it's your turn, choose wisely \n\n"
+  puts " 1 | 2 | 3             #{pos[0]} | #{pos[1]} | #{pos[2]} "
+  puts '---+---+---           ---+---+---'
+  puts " 4 | 5 | 6             #{pos[3]} | #{pos[4]} | #{pos[5]} "
+  puts '---+---+---           ---+---+---'
+  puts " 7 | 8 | 9             #{pos[6]} | #{pos[7]} | #{pos[8]} \n\n"
+  @game_spots.assign(token)
+end
 
-game.switch
+def game_setting
+  puts "Hello! who's the player one?, you will play as X.\n\n"
+  first = Player.new(gets)
+  first.char = 'X'
+  puts "Cool, and who's going to play as O?\n\n"
+  second = Player.new(gets)
+  second.char = 'O'
+  puts "Nice to meet you #{first.name.chomp} and #{second.name.chomp}. Lets begin!\n\n"
+  playing = Start.new(first, second)
+  playing.switch
+end
+
+game_setting
--- lib/core_game.rb
+++ lib/core_game.rb
@@ -1,5 +1,13 @@
+class Player
+  attr_accessor :name, :char
+  def initialize(player)
+    @char = ''
+    @name = player
+  end
+end
+
 class CoreTest
-  attr_accessor :test_res
+  attr_reader :test_res
   def initialize
     @lines = [/[1, 2, 3]/, /[4, 5, 6]/, /[7, 8, 9]/,
               /[1, 4, 7]/, /[2, 5, 8]/, /[3, 6, 9]/,
@@ -20,6 +28,54 @@ class CoreTest
       search(@x_choices)
     elsif @o_choices.length > 3
       search(@o_choices)
+    end
+  end
+end
+
+class Spots
+  attr_accessor :pos, :spots_arr, :choice
+  def initialize
+    @spots_arr = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    @ocuped = []
+    @choice = 0
+  end
+
+  def assign(token)
+    @invalid = true
+    while @invalid == true
+      @choice = gets
+      if @choice.to_i.zero? || @ocuped.any? { |u| u == @choice } || @choice.to_i.between?(1, 9) == false
+        wrong_input
+        redo
+      else
+        @ocuped << @choice
+        @spots_arr[@choice.to_i - 1] = token
+      end
+      return @invalid = false
+    end
+  end
+end
+
+class Start
+  attr_writer :player_one, :player_two
+  def initialize(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
+  end
+
+  def switch
+    @game_spots = Spots.new
+    @check_if_win = CoreTest.new
+    between = [@player_one, @player_two].cycle
+    i = 0
+    while i < 9
+      @turn = between.next
+      map(@turn.name, @turn.char, @game_spots.spots_arr)
+      @check_if_win.checker(@game_spots.choice, @turn.char)
+      return winner_msg(@turn.name) if @check_if_win.test_res == true
+
+      i += 1
+      tie_message if i == 9
     end
   end
 end
